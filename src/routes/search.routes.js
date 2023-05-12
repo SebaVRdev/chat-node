1aa54da15ad8edcd743adbbc65aced845cee37c9
SebaVRdev <sebarm177@gmail.com>
2023-05-11 23:06:56 -0400

    refactor: :fire: Eliminacion de ficheros que no son ocupados

--- src/routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const { search } = require('../controllers/search.controller');
-
-const router = Router();
-
-// /productos/oreo  <- Lo que intentara buscar en la colecion de productos todos aquellos que coincidan con "oreo"
-router.get('/:collection/:term', search)
-
-module.exports = router;
